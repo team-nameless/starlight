8491733fdf20e6d356a659b8657fe8f5befbe1b9
Tien Dat Pham <dat20036@gmail.com>
2024-11-24 18:16:36 +0700

    Remove Byte Order Mark (BOM) in GameApp.jsx

--- Starlight.Frontend/starlight/src/game/GameApp.jsx
+++ Starlight.Frontend/starlight/src/game/GameApp.jsx
@@ -1,4 +1,4 @@
-﻿import Phaser from "phaser";
+import Phaser from "phaser";
 import { useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import MainScene from "./scenes/MainScene.js";
