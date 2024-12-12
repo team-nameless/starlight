d320504d29b4401d128dd919b2a1863ae47d06a2
Tien Dat Pham <dat20036@gmail.com>
2024-12-12 18:11:58 +0700

    Make /api/game endpoint be dummy.

--- Starlight.Backend/Controller/GameController.cs
+++ Starlight.Backend/Controller/GameController.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Starlight.Backend.Controller.Request;
-using Starlight.Backend.Database.Game;
-using Starlight.Backend.Service;
 
 namespace Starlight.Backend.Controller;
 
@@ -12,42 +8,13 @@ namespace Starlight.Backend.Controller;
 [ApiController]
 public class GameController : ControllerBase
 {
-    private GameDatabaseService _gameDatabase;
-
-    public GameController(GameDatabaseService gameDatabase)
-    {
-        _gameDatabase = gameDatabase;
-    }
-    
     /// <summary>
     ///     Starts a game.
     /// </summary>
     [HttpPost("start")]
     [Authorize]
-    public async Task<ActionResult> StartGame([FromBody] GameStartRequest request)
+    public ActionResult StartGame([FromBody] GameStartRequest _)
     {
-        var services = HttpContext.RequestServices;
-        var signInManager = services.GetRequiredService<SignInManager<Player>>();
-        var userManager = signInManager.UserManager;
-        var player = await userManager.GetUserAsync(User);
-
-        var foundPlayer = await _gameDatabase.Users.FirstAsync(p => p.SequenceNumber == player!.SequenceNumber);
-
-        foundPlayer.RecentlyPlayedSong = request.SongId;
-
-        await _gameDatabase.SaveChangesAsync();
         return Ok();
-    }
-
-    /// <summary>
-    ///     Ends the game session. This endpoint simply redirect to you to the result page.
-    ///     This endpoint is intended for working inside the game.
-    /// </summary>
-    /// <returns></returns> 
-    [HttpGet("end")]
-    [Authorize]
-    public ActionResult EndGame(ulong songId)
-    {
-        return Redirect($"https://starlight.swyrin.id.vn/history/{songId}");
     }
 }
