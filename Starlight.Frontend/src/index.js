61ccd34dace76ffd44752a22d55c0afe84d437cd
Tien Dat Pham <dat20036@gmail.com>
2024-12-12 21:12:05 +0700

    Use `react-cookie` to implement "already logged in" feature.

--- Starlight.Frontend/src/index.js
+++ Starlight.Frontend/src/index.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 import './index.css';
 import './styleoflandingpage.css';
 import './Main_Menu_Style.css';
@@ -17,18 +18,19 @@ const GameApp = lazy(() => import('./game/GameApp'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            <Route path="/" element={<LandingPageApp />} />
-            <Route path="/songpage" element={<SongPage />} />
-            <Route path="/historypage/:songId/:songIndex" element={<HistoryPage />} />
-            <Route path="/storepage" element={<StorePage />} />
-            <Route path="/eventpage" element={<EventPage />} />
-            <Route path="/ProfilePage" element={<ProfilePage />} />
-            <Route path="/TestGame" element={<GameApp />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <CookiesProvider>
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<LandingPageApp />} />
+                    <Route path="/songpage" element={<SongPage />} />
+                    <Route path="/historypage/:songId/:songIndex" element={<HistoryPage />} />
+                    <Route path="/storepage" element={<StorePage />} />
+                    <Route path="/eventpage" element={<EventPage />} />
+                    <Route path="/ProfilePage" element={<ProfilePage />} />
+                    <Route path="/TestGame" element={<GameApp />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    </CookiesProvider>
 );
