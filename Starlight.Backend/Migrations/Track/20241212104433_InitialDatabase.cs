3c78e06970fd043a23214122703ceba4ac1710c9
Tien Dat Pham <dat20036@gmail.com>
2024-12-12 18:10:08 +0700

    The Tracks database is now seeded.

--- Starlight.Backend/Migrations/Track/20241212104433_InitialDatabase.cs
+++ Starlight.Backend/Migrations/Track/20241212104433_InitialDatabase.cs
@@ -2,6 +2,8 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace Starlight.Backend.Migrations.Track
 {
     /// <inheritdoc />
@@ -30,6 +32,17 @@ namespace Starlight.Backend.Migrations.Track
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Tracks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tracks",
+                columns: new[] { "Id", "Artist", "AudioFileLocation", "BackgroundFileLocation", "DataFileLocation", "Difficulty", "DifficultyFavorText", "Duration", "NoteDesigner", "Source", "Title" },
+                values: new object[,]
+                {
+                    { 586954ul, "AK X LYNX feat. Veela", "static/586954/AK x LYNX ft. Veela - Virtual Paradise.mp3", "static/586954/Bgparadise.jpg", "static/586954/586954.json", 2.7000000000000002, "HD", 88276ul, "Hydria", "N/A", "Virtual Paradise (Cut Ver.)" },
+                    { 2128243ul, "DAOKO x Kenshi Yonezu", "static/2128243/audio.mp3", "static/2128243/bg.jpg", "static/2128243/2128243.json", 1.1000000000000001, "EZ", 87500ul, "Sympho", "打ち上げ花火、下から見るか？横から見るか？", "Uchiage Hanabi (Cut Ver.)" },
+                    { 2211127ul, "Alya (CV: Uesaka Sumire)", "static/2211127/audio.ogg", "static/2211127/3.jpg", "static/2211127/2211127.json", 1.3999999999999999, "EZ", 86219ul, "keksikosu", "時々ボソッとロシア語でデレる隣のアーリャさん", "Ichiban Kagayaku Hoshi" },
+                    { 2212131ul, "Alya (CV: Uesaka Sumire)", "static/2212131/chuowo.ogg", "static/2212131/alya.jpg", "static/2212131/2212131.json", 2.1000000000000001, "NM", 88518ul, "Damaree", "時々ボソッとロシア語でデレる隣のアーリャさん", "Kawaikute Gomen" }
                 });
         }
 
--- Starlight.Backend/Service/TrackDatabaseService.cs
+++ Starlight.Backend/Service/TrackDatabaseService.cs
@@ -10,7 +10,6 @@ public class TrackDatabaseService : DbContext
 
     public TrackDatabaseService(DbContextOptions<TrackDatabaseService> options) : base(options)
     {
-        Database.EnsureCreated();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -29,5 +28,71 @@ public class TrackDatabaseService : DbContext
                     }
                 )
         );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder
+            .Entity<Track>()
+            .HasData(
+                new Track
+                {
+                    Id = 586954,
+                    Title = "Virtual Paradise (Cut Ver.)",
+                    Artist = "AK X LYNX feat. Veela",
+                    Source = "N/A",
+                    NoteDesigner = "Hydria",
+                    Difficulty = 2.7,
+                    DifficultyFavorText = "HD",
+                    Duration = 88276,
+                    DataFileLocation = "static/586954/586954.json",
+                    BackgroundFileLocation = "static/586954/Bgparadise.jpg",
+                    AudioFileLocation = "static/586954/AK x LYNX ft. Veela - Virtual Paradise.mp3"
+                },
+                new Track
+                {
+                    Id = 2128243,
+                    Title = "Uchiage Hanabi (Cut Ver.)",
+                    Artist = "DAOKO x Kenshi Yonezu",
+                    Source = "打ち上げ花火、下から見るか？横から見るか？",
+                    NoteDesigner = "Sympho",
+                    Difficulty = 1.1,
+                    DifficultyFavorText = "EZ",
+                    Duration = 87500,
+                    DataFileLocation = "static/2128243/2128243.json",
+                    BackgroundFileLocation = "static/2128243/bg.jpg",
+                    AudioFileLocation = "static/2128243/audio.mp3"
+                },
+                new Track
+                {
+                    Id = 2211127,
+                    Title = "Ichiban Kagayaku Hoshi",
+                    Artist = "Alya (CV: Uesaka Sumire)",
+                    Source = "時々ボソッとロシア語でデレる隣のアーリャさん",
+                    NoteDesigner = "keksikosu",
+                    Difficulty = 1.4,
+                    DifficultyFavorText = "EZ",
+                    Duration = 86219,
+                    DataFileLocation = "static/2211127/2211127.json",
+                    BackgroundFileLocation = "static/2211127/3.jpg",
+                    AudioFileLocation = "static/2211127/audio.ogg"
+                },
+                new Track
+                {
+                    Id = 2212131,
+                    Title = "Kawaikute Gomen",
+                    Artist = "Alya (CV: Uesaka Sumire)",
+                    Source = "時々ボソッとロシア語でデレる隣のアーリャさん",
+                    NoteDesigner = "Damaree",
+                    Difficulty = 2.1,
+                    DifficultyFavorText = "NM",
+                    Duration = 88518,
+                    DataFileLocation = "static/2212131/2212131.json",
+                    BackgroundFileLocation = "static/2212131/alya.jpg",
+                    AudioFileLocation = "static/2212131/chuowo.ogg"
+                }
+            );
     }
 }
