34da307223e7e9e45b627ae325cd3642634cc91e
Tien Dat Pham <dat20036@gmail.com>
2024-12-18 18:20:40 +0700

    Goodbye C#, you are horrible.

--- Starlight.Backend/Controller/Request/GameStartRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Starlight.Backend.Controller.Request;
-
-/// <summary>
-///     Represents a game start request.
-/// </summary>
-public class GameStartRequest
-{
-    public ulong SongId { get; set; }
-}
