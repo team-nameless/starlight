b9fc4be53ff77a81acaee7d22ea1f1a198203d29
Tien Dat Pham <dat20036@gmail.com>
2024-12-19 11:48:52 +0700

    Add minimal working Rust backend.

---
+++ backend/src/main.rs
@@ -0,0 +1,14 @@
+#[macro_use] extern crate rocket;
+
+use rocket::config::{Config};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+}
