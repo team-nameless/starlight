2f060e0a472d2c9fedac738d09106665948bd9ba
Tien Dat Pham <dat20036@gmail.com>
2024-12-22 12:11:24 +0700

    Add user deletion test.

--- backend/core/src/tests/test_user.rs
+++ backend/core/src/tests/test_user.rs
@@ -9,7 +9,7 @@ use crate::controllers::response::player_response::PlayerResponse;
 use crate::utils::exp::get_exp_for_level;
 
 #[rocket::async_test]
-async fn test_user() {
+async fn test_user_profile() {
     let prisma_client: PrismaClient = PrismaClient::_builder().build().await.expect("We fucked.");
     prisma_client
         ._execute_raw(raw!("TRUNCATE TABLE \"Player\" CASCADE"))
@@ -50,4 +50,44 @@ async fn test_user() {
     assert_eq!(body.level, 1);
     assert_eq!(body.exp, 0);
     assert_eq!(body.exp_needed_for_next_level, get_exp_for_level(body.level));
+}
+
+#[rocket::async_test]
+async fn test_user_delete() {
+    let prisma_client: PrismaClient = PrismaClient::_builder().build().await.expect("We fucked.");
+    prisma_client
+        ._execute_raw(raw!("TRUNCATE TABLE \"Player\" CASCADE"))
+        .exec()
+        .await
+        .unwrap();
+
+    let client = Client::tracked(rocket().await).await.unwrap();
+
+    let register_body = RegisterRequest {
+        handle: "KURWA",
+        email: "user@example.com",
+        password: "password123",
+    };
+
+    let response = client
+        .post("/api/register")
+        .json(&register_body)
+        .dispatch()
+        .await;
+
+    assert_eq!(response.status(), Status::Created);
+
+    let body = LoginRequest {
+        email: "user@example.com",
+        password: "password123",
+    };
+
+    let response = client.post("/api/login").json(&body).dispatch().await;
+    assert_eq!(response.status(), Status::Ok);
+
+    let response = client.delete("/api/user").dispatch().await;
+    assert_eq!(response.status(), Status::NoContent);
+
+    let response = client.post("/api/login").json(&body).dispatch().await;
+    assert_eq!(response.status(), Status::BadRequest);
 }
