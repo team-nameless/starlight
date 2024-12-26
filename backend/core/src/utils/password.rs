80b0dc9bfe79007ed07ec82fb3c68cfd721a9954
Tien Dat Pham <dat20036@gmail.com>
2024-12-26 10:10:17 +0700

    Prevent timing-attack within login.

--- backend/core/src/utils/password.rs
+++ backend/core/src/utils/password.rs
@@ -1,3 +1,4 @@
+use ring::constant_time::verify_slices_are_equal;
 use ring::digest::digest;
 use ring::digest::SHA512;
 
@@ -8,12 +9,14 @@ pub fn hash_password(password: &str) -> String {
 }
 
 /// Compares if the password equals to the stored hash.
-#[allow(dead_code)]
 pub fn verify_password(password: &str, hash: &str) -> bool {
     let digest = digest(&SHA512, password.as_bytes());
 
     let hashed = hex::encode(digest.as_ref());
     let origin = hash.to_string();
 
-    hashed == origin
+    match verify_slices_are_equal(hashed.as_bytes(), origin.as_bytes()) {
+        Ok(()) => true,
+        Err(_) => false,
+    }
 }
