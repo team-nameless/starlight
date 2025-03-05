9560b0569c6a76405aaa5d96762ab72e6fc5e63b
Tien Dat Pham <dat20036@gmail.com>
2025-03-05 20:32:30 +0700

    Unionize the project for now.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import renderer from "vite-plugin-electron-renderer";
-import electron from "vite-plugin-electron/simple";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        electron({
-            main: {
-                entry: "electron/bootstrap.ts"
-            }
-        }),
-        renderer()
+        react()
     ]
 });
