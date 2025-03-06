5b1b044d3f75506f3bcb2f8048915069a0f6d668
swyrin <41561710+swyrin@users.noreply.github.com>
2025-03-06 13:22:31 +0000

    [ci skip] Automated code format commit.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -26,9 +26,9 @@ export default defineConfig({
         })
     ],
     server: {
-        port: 3000,
+        port: 3000
     },
     build: {
-        minify: true,
+        minify: true
     }
 });
