c54965694c08d8af0236a02464c5644d800e8a07
Tien Dat Pham <dat20036@gmail.com>
2025-03-07 14:40:36 +0700

    Remove remants of 🍝 and Create React App.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,12 @@
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-        rules: {
-            "react/react-in-jsx-scope": "off",
-            "@typescript-eslint/no-unused-vars": "off"
-        }
-    }
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
